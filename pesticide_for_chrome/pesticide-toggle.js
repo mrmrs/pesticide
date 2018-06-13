1e5b12f9f46b6fc5582c85d35438ee008b6cbd8e
Rob Nagle <rob@bitmatica.com>
2018-06-12 17:21:23 -0700

    Make toggleAssets a func of a document; call for each iframe document

--- pesticide_for_chrome/pesticide-toggle.js
+++ pesticide_for_chrome/pesticide-toggle.js
@@ -1,20 +1,28 @@
-if (document.getElementById("pesticideCSS") && document.getElementById("pesticideJS")) {
-  document.getElementsByTagName("head")[0].removeChild(document.getElementById("pesticideCSS"));
-  document.getElementsByTagName("head")[0].removeChild(document.getElementById("pesticideJS"));
-  document.getElementsByTagName("body")[0].removeChild(document.getElementById("pesticide-for-chrome-result"));
-} else {
-  pesticideCSS = document.createElement("link");
-  pesticideCSS.rel = "stylesheet";
-  pesticideCSS.type = "text/css";
-  pesticideCSS.href = chrome.extension.getURL("/pesticide.min.css");
-  pesticideCSS.id = "pesticideCSS";
-  document.getElementsByTagName("head")[0].appendChild(pesticideCSS);
-  pesticideJS = document.createElement("script");
-  pesticideJS.type = "text/javascript";
-  pesticideJS.src = chrome.extension.getURL("/pesticide.js");
-  pesticideJS.id = "pesticideJS";
-  document.getElementsByTagName("head")[0].appendChild(pesticideJS);
-  pesticideResult = document.createElement("div"),
-  pesticideResult.id = "pesticide-for-chrome-result",
-  document.getElementsByTagName("body")[0].appendChild(pesticideResult)
+function toggleAssets(doc) {
+  if (doc.getElementById("pesticideCSS") && doc.getElementById("pesticideJS")) {
+    doc.getElementsByTagName("head")[0].removeChild(doc.getElementById("pesticideCSS"));
+    doc.getElementsByTagName("head")[0].removeChild(doc.getElementById("pesticideJS"));
+    doc.getElementsByTagName("body")[0].removeChild(doc.getElementById("pesticide-for-chrome-result"));
+  } else {
+    pesticideCSS = doc.createElement("link");
+    pesticideCSS.rel = "stylesheet";
+    pesticideCSS.type = "text/css";
+    pesticideCSS.href = chrome.extension.getURL("/pesticide.min.css");
+    pesticideCSS.id = "pesticideCSS";
+    doc.getElementsByTagName("head")[0].appendChild(pesticideCSS);
+    pesticideJS = doc.createElement("script");
+    pesticideJS.type = "text/javascript";
+    pesticideJS.src = chrome.extension.getURL("/pesticide.js");
+    pesticideJS.id = "pesticideJS";
+    doc.getElementsByTagName("head")[0].appendChild(pesticideJS);
+    pesticideResult = doc.createElement("div"),
+    pesticideResult.id = "pesticide-for-chrome-result",
+    doc.getElementsByTagName("body")[0].appendChild(pesticideResult)
+  }
 }
+
+toggleAssets(document)
+Array.from(document.getElementsByTagName('iframe'))
+    .map(frame => frame.contentDocument)
+    .filter(doc => doc) // contentDocument will be null for cross-origin frames
+    .map(toggleAssets)
